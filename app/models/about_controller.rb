4d9151de6b4b534a26b72629194920e0992e5b12
SorrenJ <soalexjao@gmail.com>
2024-09-14 03:15:31 -0700

    add /about route, create erb, add about instance variable, add about controller

---
+++ app/models/about_controller.rb
@@ -0,0 +1,8 @@
+class AboutController < ApplicationController
+    def index
+      @about_description = "A fully functional mock e-commerce website built with ruby on rails, along with a SQL database. Uses a Stripe Api for payment"
+     
+    
+    end
+  end
+  
